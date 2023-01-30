43f290cddeaa1e32d5c3258d0f08dfdbde8b5019
Luiz Esmiralha <>
2023-01-30 17:44:07 -0300

    add CALL to asm syntax highlight

--- lmsm-ui-vanilla.js
+++ lmsm-ui-vanilla.js
@@ -25,7 +25,7 @@ class LMSMUi {
     CodeMirror.defineSimpleMode('lmsm-assembly', {
       start: [
         {
-          regex: /(?:DAT|ADD|SUB|STA|LDI|LDA|BRA|BRZ|BRP|NOOP|INP|OUT|SPUSH|SPOP|SDUP|SDROP|SSWAP|SADD|SSUB|SMUL|SDIV|SMAX|SMIN|JAL|RET|HLT|dat|add|sub|sta|ldi|lda|bra|brz|brp|noop|inp|out|spush|spop|sdup|sdrop|sswap|sadd|ssub|smul|sdiv|smax|smin|jal|ret|hlt)\b/,
+          regex: /(?:CALL|DAT|ADD|SUB|STA|LDI|LDA|BRA|BRZ|BRP|NOOP|INP|OUT|SPUSH|SPOP|SDUP|SDROP|SSWAP|SADD|SSUB|SMUL|SDIV|SMAX|SMIN|JAL|RET|HLT)\b/,
           token: 'keyword'
         },
         {
